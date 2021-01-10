531814574b47eb21bd9c5e8635c140279f74a322
Mark Nikerin <mrnikerin@gmail.com>
2021-01-11 03:59:26 +0400

    Add DTOs

---
+++ AirportSimulationSystem/AirportSimulationSystem/Models/DTOs/AirplaneDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace AirportSimulationSystem.Models.DTOs
+{
+    using AirportSimulationSystem.Database.Entities;
+    using System.ComponentModel;
+
+    public class AirplaneDTO
+    {
+        [DisplayName("№ самолёта")]
+        public int Id { get; set; }
+
+        [DisplayName("Модель")]
+        public string Model { get; set; }
+
+        [DisplayName("Дальность")]
+        public int Distance { get; set; }
+
+        [DisplayName("Пассажировместимость")]
+        public int PassengerCapacity { get; set; }
+
+        [DisplayName("Грузоподъёмность")]
+        public int LiftingCapacity { get; set; }
+
+        public static explicit operator AirplaneDTO(Airplane entity)
+        {
+            return new AirplaneDTO
+            {
+                Id = entity.Id,
+                Model = entity.Model,
+                Distance = entity.Distance,
+                PassengerCapacity = entity.PassengerCapacity,
+                LiftingCapacity = entity.LiftingCapacity
+            };
+        }
+    }
+}
---
+++ AirportSimulationSystem/AirportSimulationSystem/Models/DTOs/FlightDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace AirportSimulationSystem.Models.DTOs
+{
+    using AirportSimulationSystem.Database.Entities;
+    using System;
+    using System.ComponentModel;
+
+    public class FlightDTO
+    {
+        [DisplayName("№ рейса")]
+        public int Id { get; set; }
+
+        [DisplayName("Рейс")]
+        public int FlightNumber { get; set; }
+
+        [DisplayName("Название")]
+        public string Title { get; set; }
+
+        [DisplayName("Время")]
+        public DateTime Time { get; set; }
+
+        [DisplayName("№ стойки регистрации")]
+        public int RegistryNumber { get; set; } 
+
+        [DisplayName("Число проданных билетов")]
+        public int SoldTicketsAmount { get; set; }
+
+        public static explicit operator FlightDTO(Flight entity)
+        {
+            return new FlightDTO
+            {
+                 Id = entity.Id,
+                 FlightNumber = entity.FlightNumber,
+                 Title = entity.Title,
+                 Time = entity.Time,
+                 RegistryNumber = entity.RegistryNumber,
+                 SoldTicketsAmount = entity.SoldTicketsAmount 
+            };
+        }
+    }
+}
